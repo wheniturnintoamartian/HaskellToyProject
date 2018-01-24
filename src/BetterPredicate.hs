7323b3bd7af0ef3eb71a568d0562d2fbae9b81f5
lalala <lalala>
2018-01-24 17:34:06 +0200

    Adding betterPredicate

--- src/BetterPredicate.hs
+++ src/BetterPredicate.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module BetterPredicate (
       betterFind
     ) where
@@ -6,19 +8,39 @@ import Control.Monad (filterM)
 import System.Directory (Permissions(..), getModificationTime, getPermissions)
 import System.Time (ClockTime(..))
 import System.FilePath (takeExtension)
-import Control.Exception (bracket, handle)
+import Control.Exception (bracket, handle, Exception, IOException)
 import System.IO (IOMode(..), hClose, hFileSize, openFile)
+
 -- the function we wrote earlier
 import RecursiveContents (getRecursiveContents)
 
 type Predicate = FilePath -> Permissions -> Maybe Integer -> ClockTime -> Bool
 
 getFileSize :: FilePath -> IO (Maybe Integer)
+getFileSize path = handle (\(_ :: IOException) -> return Nothing) $
+                        bracket (openFile path ReadMode) hClose $ \h -> do
+                            size <- hFileSize h
+                            return (Just size)
 
 betterFind :: Predicate -> FilePath -> IO [FilePath]
 betterFind p path = getRecursiveContents path >>= filterM check
     where check name = do
-        perms <- getPermissions name
-        size <- getFileSize name
-        modified <- getModificationTime name
-        return (p name perms size modified)
+            perms <- getPermissions name
+            size <- getFileSize name
+            modified <- getModificationTime name
+            return (p name perms size modified)
+
+simpleFileSize :: FilePath -> IO Integer
+simpleFileSize path = do
+    h <- openFile path ReadMode
+    size <- hFileSize h
+    hClose h
+    return size
+
+saferFileSize :: FilePath -> IO (Maybe Integer)
+saferFileSize path = handle (\(_ :: IOException) -> return Nothing) $ do
+    h <- openFile path ReadMode
+    size <- hFileSize h
+    hClose h
+    return (Just size)
+
