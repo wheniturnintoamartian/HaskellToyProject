65a1f2dfdfbf18d8d260525980449c87b40f0ac3
cuka cuka <steve.papadogiannis1992@gmail.com>
2018-01-27 15:23:33 +0200

    Adding more functions in BetterPredicate

--- src/BetterPredicate.hs
+++ src/BetterPredicate.hs
@@ -60,3 +60,29 @@ equalP f k = \w x y z -> f w x y z == k
 
 equalP' :: (Eq a) => InfoP a -> a -> InfoP Bool
 equalP' f k w x y z = f w x y z == k
+
+liftP :: (a -> b -> c) -> InfoP a -> b -> InfoP c
+liftP q f k w x y z = f w x y z `q` k
+
+greaterP, lesserP :: (Ord a) => InfoP a -> a -> InfoP Bool
+greaterP = liftP (>)
+lesserP = liftP (<)
+
+simpleAndP :: InfoP Bool -> InfoP Bool -> InfoP Bool
+simpleAndP f g w x y z = f w x y z && g w x y z
+
+liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
+liftP2 q f g w x y z = f w x y z `q` g w x y z
+
+andP = liftP2 (&&)
+orP = liftP2 (||)
+
+constP :: a -> InfoP a
+constP k _ _ _ _ = k
+
+liftP' q f k w x y z = f w x y z `q` constP k w x y z
+
+liftPath :: (FilePath -> a) -> InfoP a
+liftPath f w _ _ _ = f w
+
+myTest2 = (liftPath takeExtension `equalP` ".cpp") `andP` (sizeP `greaterP` 2)
