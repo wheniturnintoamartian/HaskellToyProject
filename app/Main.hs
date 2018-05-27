91492e018022ac007933edbc54fa3b1ce7f99dbe
stefanos <wheniturnintoamartian@gmail.com>
2018-05-27 20:14:35 +0300

    Adding code for barcode recognition.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,11 @@
 module Main where
 
+import Barcode (checkDigit)
+
 main :: IO ()
-main = putStrLn "Hello World"
+main = do  
+    args <- getArgs  forM_ args $ \arg -> do    
+        e <- parse parseRawPPM <$> L.readFile arg    
+        case e of      
+            Left err ->     print $ "error: " ++ err      
+            Right pixmap -> print $ findEAN13 pixmap 
